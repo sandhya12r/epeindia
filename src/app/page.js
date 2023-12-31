e22e4d85c0fdd192763d62894816b5ddfe22edad
shubham <sbarwad20595@gmail.com>
2023-12-31 12:33:07 +0530

    Created the form section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Header from './components/Header'
 import SliderComponent from './components/SliderComponent'
+import InquiryForm from './components/InquiryForm'
 export default function Home() {
   return (
     <main>
       <Header />
       <SliderComponent/>
+      <InquiryForm />
     </main>
   )
 }
