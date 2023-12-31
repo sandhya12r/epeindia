5f6f8ab7729e5e4fcad13ee6c76940d0c47dfc26
shubham <sbarwad20595@gmail.com>
2023-12-31 15:03:15 +0530

    updated whyepe

--- src/app/components/WhyEPE.js
+++ src/app/components/WhyEPE.js
@@ -1,21 +1,32 @@
-import React from 'react'
+    import Image from 'next/image'
+    import React from 'react'
 
-function WhyEPE() {
-  return (
-    <div className='bg-black text-white'>
-        <div className='container mx-auto py-10 flex flex-col items-center justify-center'>
-            <h3 className='text-3xl font-bold text-center mb-10 uppercase'>why e pe?</h3>
-            <div className='grid grid-cols-1 lg:grid-cols-2'>
-                <div className='bg-white'>
-                    
+    function WhyEPE() {
+    return (
+        <div className='bg-black'>
+            <div className='container mx-auto py-10 flex flex-col items-center justify-center'>
+                <h3 className='text-3xl font-bold text-center mb-10 uppercase text-white'>why e pe?</h3>
+                <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-stretch w-full gap-5'>
+                    <div className='bg-white flex items-center justify-center rounded-md'>
+                        <Image src={'https://epeindia.in/assets/designer/themes/default/images/why-ico1.png'} height={185} width={205} alt=''/>
+                        <p className='text-xl'><span className='text-green-500 font-bold'>One Stop Solution</span> for all your Investment Needs</p>
+                    </div>
+                    <div className='bg-white flex items-center justify-center rounded-md'>
+                            <Image src={'https://epeindia.in/assets/designer/themes/default/images/why-ico1.png'} height={185} width={205} alt=''/>
+                            <p className='text-xl'><span className='text-green-500 font-bold'>One Stop Solution</span> for all your Investment Needs</p>
+                    </div>
+                    <div className='bg-white flex items-center justify-center rounded-md'>
+                            <Image src={'https://epeindia.in/assets/designer/themes/default/images/why-ico1.png'} height={185} width={205} alt=''/>
+                            <p className='text-xl'><span className='text-green-500 font-bold'>One Stop Solution</span> for all your Investment Needs</p>
+                    </div>
+                    <div className='bg-white flex items-center justify-center rounded-md'>
+                        <Image src={'https://epeindia.in/assets/designer/themes/default/images/why-ico1.png'} height={185} width={205} alt=''/>
+                        <p className='text-xl'><span className='text-green-500 font-bold'>One Stop Solution</span> for all your Investment Needs</p>
+                    </div>
                 </div>
-                <div className='bg-white'></div>
-                <div className='bg-white'></div>
-                <div className='bg-white'></div>
             </div>
         </div>
-    </div>
-  )
-}
+    )
+    }
 
-export default WhyEPE
+    export default WhyEPE
